4cc8fb50c915aeea6b916648f3a20281c262465c
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-27 10:07:31 -0500

    removed extra input field

--- client.js
+++ client.js
@@ -4,9 +4,9 @@ myApp.controller('GuessWhoController',function(){
   var guess = this;
   guess.people = [{name: 'Aaron',github:'bozeman42', show: true},{name: "Emi", github: "emimbchen", show: true},{name: "Marta", github: "martajopp", show: true},{name: "Xong", github: "xongxiong", show: true},{name: "Chris Black", github: "christopher-black", show: true}];
 
-  guess.submitName = function(){
+  guess.submitName = function(nameIn,githubIn){
     guess.people.push({name: nameIn,github: githubIn, show: true});
-    nameIn = '';
+    guess.nameIn = '';
     guess.githubIn = '';
     console.log(guess.people);
   };
